66dfd6d709fad0800da3c1f605748eefb2a4b16f
nicmosc <nmoscholios@gmail.com>
2017-06-10 20:59:06 +0200

    try using height instead of speed to determine paddle speed

--- app/utils/index.js
+++ app/utils/index.js
@@ -5,19 +5,31 @@ export function ai(ball, paddle) {
   const { x, y, ySpeed } = ball;
   const { height, speed } = constants;
 
-  const absoluteSpeed = Math.abs(ySpeed);
-  const aiSpeed = speed + 10;
+  const ballSpeed = Math.abs(ySpeed);
 
   if (y >= paddle.y + height / 2) {
-    if (absoluteSpeed > aiSpeed) {
-      return paddle.y + aiSpeed;
+    if (ballSpeed > height / 2) {
+      return paddle.y + speed;
     }
-    return paddle.y + absoluteSpeed;
+    return paddle.y + ballSpeed;
   }
   else {
-    if (absoluteSpeed > aiSpeed) {
-      return paddle.y - aiSpeed;
+    if (ballSpeed > height / 2) {
+      return paddle.y - speed;
     }
-    return paddle.y - absoluteSpeed;
+    return paddle.y - ballSpeed;
   }
+
+  // if (y >= paddle.y + height / 2) {
+  //   if (y + ballSpeed < paddle.y + height / 2 || (ySpeed > 0 && y + ballSpeed > paddle.y + height / 2)) {
+  //     return paddle.y + speed;
+  //   }
+  //   return paddle.y + ballSpeed;
+  // }
+  // else {
+  //   if (y + ballSpeed < paddle.y + height / 2 || (ySpeed < 0 && y + ballSpeed < paddle.y + height / 2)) {
+  //     return paddle.y - speed;
+  //   }
+  //   return paddle.y - ballSpeed;
+  // }
 }
